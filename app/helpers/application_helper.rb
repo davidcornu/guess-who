a5f2f028a60c732548704fe037a9cecc96229df4
chloepouprom <Chloe@Chloes-MacBook-Air.local>
2014-05-29 13:38:00 -0400

    modified colours for pie chart

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def chart_colors
     {
-      'male'    => 'blue',
-      'female'  => 'green',
-      'neutral' => 'purple'
+      'male'    => '#00baf2',
+      'female'  => '#fe4d53',
+      'neutral' => '#d766fc'
     }
   end
 end
